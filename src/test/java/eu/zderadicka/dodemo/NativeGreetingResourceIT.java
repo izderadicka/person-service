909f311551afbd8ca7204e22dab89ab4335b0796
Ivan <ivan.zderadicka@gmail.com>
2022-04-12 13:15:30 +0200

    Initial version

---
+++ src/test/java/eu/zderadicka/dodemo/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package eu.zderadicka.dodemo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
